fc9d67e7fe6d548624463da81d419b74e85686e5
naufalaml <achmadnaufal873@gmail.com>
2023-11-22 16:08:40 +0700

    y

---
+++ conf/read.php
@@ -0,0 +1,27 @@
+<?php 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $kode_pj = $_POST['kode_pj'];
+    $kode_anggota = $_POST['kode_anggota'];
+    $tanggal = $_POST['tanggal'];
+    $jumlah = $_POST['jumlah'];
+    $bunga = $_POST['bunga'];
+    $status = $_POST['status'];}
+    try {
+        $query = "INSERT INTO pinjaman (kode_pj, kode_anggota, tanggal, jumlah, bunga, status) 
+                  VALUES (:kode_pj, :kode_anggota, :tanggal, :jumlah, :bunga, :status)";
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(':kode_pj', $kode_pj);
+        $stmt->bindParam(':kode_anggota', $kode_anggota);
+        $stmt->bindParam(':tanggal', $tanggal);
+        $stmt->bindParam(':jumlah', $jumlah);
+        $stmt->bindParam(':bunga', $bunga);
+        $stmt->bindParam(':status', $status);
+
+        $stmt->execute();
+
+
+        exit();
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+?>
--- conf/upload.php
+++ conf/upload.php
@@ -23,9 +23,11 @@ if ($_POST) {
                 $hp = $db->real_escape_string(htmlspecialchars($_POST['agt_nohp']));
                 $ktp = $db->real_escape_string(htmlspecialchars($namaAcak));
                 $tglmasuks = $db->real_escape_string(htmlspecialchars($tglmasuk));
-
-                $sql = "INSERT INTO nasabah VALUES('', '$kode', '$nama', '$nik', '$alamat', '$hp', '$ktp','$tglmasuks')";
-                $result = $db->query($sql);
+                
+                $sql = "INSERT INTO nasabah (kode, nama, nik, alamat, hp, ktp, tglmasuk) VALUES (?, ?, ?, ?, ?, ?, ?)";
+                $stmt = $db->prepare($sql);
+                $stmt->bind_param("sssssss", $kode, $nama, $nik, $alamat, $hp, $ktp, $tglmasuks);
+                $result = $stmt->execute();
                 if ($result) {
                     $respon['respon'] = 'data berhasil tersimpan';
                 } else {
