40c6a4498c70612501b7c278373f519e7dd7c111
Peterson <151447587+Tuanpeterson@users.noreply.github.com>
2023-11-23 00:52:04 +0700

    t

---
+++ conf/read_nasabah.php
@@ -0,0 +1,50 @@
+<?php
+require_once 'connect.php';
+$sql = "select count(*) as total from nasabah";
+$result = $db->query($sql);
+$row = $result->fetch_assoc();
+$totalrecords = $row['total'];
+
+
+$limit = $_GET['length'];
+$start = $_GET['start'];
+$orderColumnIndex = $_GET['order'][0]['column'];
+$orderDir = $_GET['order'][0]['dir'];
+
+$columns = ['agt_id','agt_kode','agt_nama','agt_nik','agt_alamat','agt_nohp',
+'agt_ktp','tanggal_register'];
+$orderBy = $columns[$orderColumnIndex];
+
+$sql = "select * from nasabah";
+
+$searchValue = $_GET['search']['value'];
+
+if (!empty($searchValue)) {
+    
+    $sql .= " WHERE agt_nama LIKE '%$searchValue%'
+     OR agt_nik LIKE '%$searchValue%' 
+     or agt_alamat like '%$searchValue%' 
+    or agt_nohp like '%$searchValue%'";
+}
+
+$sql .= " order by $orderBy $orderDir limit $start, $limit";
+$result = $db->query($sql);
+$totalrecordsFiltered =$result->num_rows;
+
+$no = 1;
+
+$data = array();
+while ($row = $result->fetch_assoc()) {
+    // $row['index']=$no++;
+    $data[]=$row;
+};
+
+$output = [
+    "draw"=>intval($_GET['draw']),
+    'recordsTotal'=>$totalrecords,
+    'recordsFiltered'=>$totalrecordsFiltered,
+    'data'=>$data
+];
+
+header('Content-Type: application/json');
+echo json_encode($output);
