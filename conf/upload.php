4d71c9e4a5202be8a4c10a2dfd68f69f6ffd622f
Peterson <151447587+Tuanpeterson@users.noreply.github.com>
2023-11-23 00:43:29 +0700

    t

--- conf/upload.php
+++ conf/upload.php
@@ -1,49 +1,65 @@
 <?php
+header("Content-Type: application/json; charset=UTF-8");
 $respon = [];
 
-if ($_POST) {
-    if ($_FILES['file']['error'] === UPLOAD_ERR_OK) {
-        $validate = ['jpg', 'png'];
-        $nama = explode('.', $_FILES['file']['name']);
-        if (in_array(end($nama), $validate)) {
-            if ($_FILES['file']['size'] < 10 * 1024 * 1024) {
-                $namaAcak = md5(uniqid(mt_rand(), true)) . '.' . end($nama);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!isset($_POST['agt_kode'])) {
+        $respon['respon']='kode wajib diisi';
+    } elseif (!isset($_POST['agt_nama'])) {
+        
+        $respon['respon']='nama wajib diisi';
+    } elseif (!isset($_POST['agt_nik'])) {
+        $respon['respon']='nik wajib diisi';
+    } elseif (!isset($_POST['agt_alamat'])) {
+        $respon['respon']='alamat wajib diisi';
+    } elseif (!isset($_POST['agt_nohp'])) {
+        $respon['respon']='nomer telepon wajib diisi';
+    } else {
+        if (!empty($_FILES['file']['name'])) {
+            if ($_FILES['file']['error'] === UPLOAD_ERR_OK) {
+                $validate = ['jpg', 'png'];
+                $nama = explode('.', $_FILES['file']['name']);
+                $file_extension = strtolower(end($nama));
+                if (in_array($file_extension, $validate)) {
+                    if ($_FILES['file']['size'] < 10 * 1024 * 1024) {
+                        $namaAcak = md5(uniqid(mt_rand(), true)) . '.' . $file_extension;
 
-                $tglmasuk = date('Y-m-d');
+                        require_once 'connect.php';
+                        
+                        $tempFile = $_FILES['file']['tmp_name'];
+                        $targetPath = '../uploads/';
+                        move_uploaded_file($tempFile, $targetPath . $namaAcak);
+                        $kode = $db->real_escape_string(htmlspecialchars($_POST['agt_kode']));
+                        $nama = $db->real_escape_string(htmlspecialchars($_POST['agt_nama']));
+                        $nik = $db->real_escape_string(htmlspecialchars($_POST['agt_nik']));
+                        $alamat = $db->real_escape_string(htmlspecialchars($_POST['agt_alamat']));
+                        $hp = $db->real_escape_string(htmlspecialchars($_POST['agt_nohp']));
+                        $ktp = $db->real_escape_string(htmlspecialchars($namaAcak));
+                        $tglmasuks = $db->real_escape_string(htmlspecialchars(date('Y-m-d')));
 
-                require_once 'connect.php';
-                $tempFile = $_FILES['file']['tmp_name'];
-                $targetPath = '../uploads/';
-                move_uploaded_file($tempFile, $targetPath . $namaAcak);
-
-                $kode = $db->real_escape_string(htmlspecialchars($_POST['agt_kode']));
-                $nama = $db->real_escape_string(htmlspecialchars($_POST['agt_nama']));
-                $nik = $db->real_escape_string(htmlspecialchars($_POST['agt_nik']));
-                $alamat = $db->real_escape_string(htmlspecialchars($_POST['agt_alamat']));
-                $hp = $db->real_escape_string(htmlspecialchars($_POST['agt_nohp']));
-                $ktp = $db->real_escape_string(htmlspecialchars($namaAcak));
-                $tglmasuks = $db->real_escape_string(htmlspecialchars($tglmasuk));
-
-
-                $sql = "INSERT INTO nasabah VALUES('', '$kode', '$nama', '$nik', '$alamat', '$hp', '$ktp','$tglmasuks')";
-                $result = $db->query($sql);
-                if ($result) {
-                    $respon['respon'] = 'data berhasil tersimpan';
+                        $sql = "INSERT INTO nasabah VALUES('', '$kode', '$nama', '$nik', '$alamat', '$hp', '$ktp','$tglmasuks')";
+                        $result = $db->query($sql);
+                        if ($result) {
+                            $respon['respon'] = 'data berhasil tersimpan';
+                        } else {
+                            $respon['respon'] = 'gagal menyimpan data';
+                        }
+                        
+                    } else {
+                        $respon['respon'] = 'ukuran maksimal 5 mb';
+                    }
                 } else {
-                    $respon['respon'] = 'gagal menyimpan data';
+                    $respon['respon'] = 'extensi file tidak sesuai';
                 }
             } else {
-                $respon['respon'] = 'ukuran maksimal 5 mb';
+                $respon['respon'] = 'upload gagal';
             }
         } else {
-            $respon['respon'] = 'extensi file tidak sesuai';
+            $respon['respon'] = 'ktp wajib diisi';
         }
-    } else {
-        $respon['respon'] = 'upload gagal';
+    
     }
 } else {
     $respon['respon'] = 'method tidak sesuai';
 }
-
-header("Content-Type: application/json; charset=UTF-8");
 echo json_encode($respon);
