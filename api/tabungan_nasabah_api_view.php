4614ced71707f469c5e8790106fc379943dee5ef
Teguh Saint <barayacoffeejombang@gmail.com>
2023-12-03 04:23:56 +0700

    pair tabungan

--- api/tabungan_nasabah_api_view.php
+++ api/tabungan_nasabah_api_view.php
@@ -8,7 +8,7 @@ $db_password = "";
 $db_name = "db_koperasi";
 $conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 
-$query = "SELECT * FROM tabungan_nasabah";
+$query = "SELECT * FROM tabungan JOIN nasabah ON nasabah.agt_kode = tabungan.kode_agt";
 $res = mysqli_query($conn, $query);
 
 $html = "";
@@ -19,7 +19,7 @@ foreach ($res as $key) {
 
 <td>" . $key['kode_agt'] . "</td>
 <td>" . $key['tanggal'] . "</td>
-<td>" . $key['jumlah'] . "</td>
+<td>Rp." . number_format($key['jumlah']) . "</td>
 <td>" . $key['agt_nama'] . "</td>
 <td><button onclick=\"button_muncul('".$key['id_tbg']."','". $key['kode_agt']."','". $key['tanggal']."','". $key['jumlah']."','" . $key['agt_nama'] ."')\" id='tombolOption' class='btn btn-sm btn-success' data-id=''><i class='fas fa-cog'></i></button></td>
 </tr>";
--- assets/js/route.js
+++ assets/js/route.js
@@ -2,29 +2,27 @@
 // Contoh : <a href="javascript:void(0)" data-halaman="home.html" class="linkpage"></a>
 
 var url_folder_api = "http://localhost/bina-Koperasi/Bina-Koperasi/";
-$(document).ready(function () {
-  var halama_utama = "home.html";
+var halama_utama = "home.html";
 
-  var halaman_saatini = "";
-  var dir_halaman = "pages/";
-  var selector_loadHalaman = $("main");
+var halaman_saatini = "";
+var dir_halaman = "pages/";
+var selector_loadHalaman = $("main");
 
-  if (localStorage.getItem("halaman_tersimpan")) {
-    halaman_saatini = localStorage.getItem("halaman_tersimpan");
-    load_halaman(halaman_saatini);
-  } else {
-    halaman_saatini = halama_utama;
-    localStorage.setItem("halaman_tersimpan", halaman_saatini);
-    load_halaman(halaman_saatini);
-  }
-  $(".linkpage").on("click", function () {
-    halaman_saatini = $(this).attr("data-halaman");
-    localStorage.setItem("halaman_tersimpan", halaman_saatini);
-    load_halaman(halaman_saatini);
-  });
-  function load_halaman(halaman_apa) {
-    $(selector_loadHalaman).hide();
-    $(selector_loadHalaman).load(dir_halaman + halaman_apa);
-    $(selector_loadHalaman).fadeIn(300);
-  }
+if (localStorage.getItem("halaman_tersimpan")) {
+  halaman_saatini = localStorage.getItem("halaman_tersimpan");
+  load_halaman(halaman_saatini);
+} else {
+  halaman_saatini = halama_utama;
+  localStorage.setItem("halaman_tersimpan", halaman_saatini);
+  load_halaman(halaman_saatini);
+}
+$(".linkpage").on("click", function () {
+  halaman_saatini = $(this).attr("data-halaman");
+  localStorage.setItem("halaman_tersimpan", halaman_saatini);
+  load_halaman(halaman_saatini);
 });
+function load_halaman(halaman_apa) {
+  $(selector_loadHalaman).hide();
+  $(selector_loadHalaman).load(dir_halaman + halaman_apa);
+  $(selector_loadHalaman).fadeIn(300);
+}
